b8402955c903b10a957dad0fbadc40dba288d307
Daley Mikalson <daley.mikalson@gmail.com>
2017-07-02 15:12:05 -0400

    Commenting
    More descriptive comments for each plot and small fixes to legends and variable names.

--- import.R
+++ import.R
@@ -1,4 +1,4 @@
-# Get data
+# Get data (exported from the City of Toronto cycling app)
 folder <- "~/Documents/GIS_data/Bike_Trips/"
 file_list <- list.files(path=folder, pattern = "*.csv")
 biketrips <-
@@ -7,28 +7,34 @@ biketrips <-
                  function(x)
                    read.csv(paste0(folder, x), stringsAsFactors = FALSE)))
 
+# Pasre date-times
 library(lubridate)
-biketrips$X <- ymd_hms(paste(biketrips$Date, biketrips$Time, sep = " "))
+biketrips$date_time <- ymd_hms(paste(biketrips$Date, biketrips$Time, sep = " "))
 
+# Get subset of today's trips only
 today_trip <- subset(biketrips, Date == Sys.Date())
 
-#plots
+# Plot all trips in record and export as PNG
 library(ggplot2)
 p <- ggplot(biketrips, aes(Longitude, Latitude, colour = Date))
 p <- p + geom_point()
 p
 ggsave("all.png", dpi = 300, width = 10, height  = 6)
 
-q <- ggplot(biketrips, aes(Longitude, Latitude, colour = year(Date)))
-q + geom_point()
+# Plot trips by year
+q <- ggplot(biketrips, aes(Longitude, Latitude, colour = as.factor(year(Date))))
+q + geom_point() + guides(colour = guide_legend(title = "Year"))
 
-r <- ggplot(biketrips, aes(Longitude, Latitude, colour = am(X)))
-r + geom_point()
+# Plot showing which parts of trips were in the morning vs. the afternoon
+r <- ggplot(biketrips, aes(Longitude, Latitude, colour = am(date_time)))
+r + geom_point() + guides(colour = guide_legend(title = "Morning Ride?"))
 
+# Plot just today's trip
 p2 <- ggplot(today_trip, aes(Longitude, Latitude))
 p2 <- p2 + geom_point()
 p2
 
+# Plot with all trips in grey in the background and today's trip in colour
 p <- ggplot(biketrips, aes(Longitude, Latitude)) + geom_point()
 p + geom_point(data = today_trip, aes(Longitude, Latitude, colour = Date)) + 
   theme(legend.position="none")
