5dc3e548d8f39842b0234b83aa01a42eb9bd3486
Daley Mikalson <daley.mikalson@gmail.com>
2017-09-12 15:04:34 -0400

    Added coord_map() to plots

--- import.R
+++ import.R
@@ -19,29 +19,31 @@ biketrips <- biketrips %>%
 today_trip <- biketrips %>% filter(Date == Sys.Date())
 
 # Plot all trips in record and export as PNG
-all_trips <- ggplot(biketrips, aes(Longitude, Latitude, colour = Date)) + geom_point()
+all_trips <- ggplot(biketrips, aes(Longitude, Latitude, colour = Date)) + 
+  geom_point() + coord_map()
 all_trips
 ggsave("all.png", dpi = 300, width = 10, height  = 6)
 
 # Plot trips by year
 by_year <- ggplot(biketrips, 
             aes(Longitude, Latitude, colour = as.factor(year(Date)))) +
-  geom_point() + guides(colour = guide_legend(title = "Year"))
+  geom_point() + guides(colour = guide_legend(title = "Year"))  + coord_map()
 by_year
 
 # Plot showing which parts of trips were in the morning vs. the afternoon
 am_pm_trips <- ggplot(biketrips, aes(Longitude, Latitude, colour = am(date_time))) + 
-  geom_point() + 
-  guides(colour = guide_legend(title = "Morning Ride?"))
+  geom_point() + coord_map() +
+  guides(colour = guide_legend(title = "Morning Ride?")) 
 am_pm_trips
 
 # Plot just today's trip
-todays_ride <- ggplot(today_trip, aes(Longitude, Latitude)) + geom_point()
+todays_ride <- ggplot(today_trip, aes(Longitude, Latitude)) + 
+  geom_point() + coord_map()
 todays_ride
 
 # Plot with all trips in grey in the background and today's trip in colour
 focus_today <- ggplot(biketrips, aes(Longitude, Latitude)) + geom_point() + 
   geom_point(data = today_trip, aes(Longitude, Latitude, colour = Date)) + 
-  theme(legend.position="none")
+  theme(legend.position="none") + coord_map()
 focus_today
 ggsave("today_overlay.png", dpi = 300, width = 8, height  = 6)
