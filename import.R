c4cf76179f2e9c23669d194b48085eeaadd5f0ee
Daley Mikalson <daley.mikalson@gmail.com>
2018-01-04 21:28:33 -0500

    change colour palette

--- import.R
+++ import.R
@@ -1,6 +1,7 @@
 # Libraries
 library(tidyverse)
 library(lubridate)
+library(viridis)
 
 # Get data (exported from the City of Toronto cycling app)
 folder <- "~/Documents/GIS_data/Bike_Trips/"
@@ -20,20 +21,23 @@ today_trip <- biketrips %>% filter(Date == Sys.Date())
 
 # Plot all trips in record and export as PNG
 all_trips <- ggplot(biketrips, aes(Longitude, Latitude, colour = Date)) + 
-  geom_point() + coord_map()
+  geom_point() + coord_map() +
+  scale_colour_viridis(discrete = TRUE)
 all_trips
 ggsave("all.png", dpi = 300, width = 10, height  = 6)
 
 # Plot trips by year
 by_year <- ggplot(biketrips, 
             aes(Longitude, Latitude, colour = as.factor(year(Date)))) +
-  geom_point() + guides(colour = guide_legend(title = "Year"))  + coord_map()
+  geom_point() + guides(colour = guide_legend(title = "Year"))  + coord_map() +
+  scale_colour_viridis(discrete = TRUE, alpha = 0.5)
 by_year
 
 # Plot showing which parts of trips were in the morning vs. the afternoon
 am_pm_trips <- ggplot(biketrips, aes(Longitude, Latitude, colour = am(date_time))) + 
   geom_point() + coord_map() +
-  guides(colour = guide_legend(title = "Morning Ride?")) 
+  guides(colour = guide_legend(title = "Morning Ride?")) +
+  scale_colour_viridis(discrete = TRUE, alpha = 0.5)
 am_pm_trips
 
 # Plot just today's trip
@@ -44,7 +48,8 @@ todays_ride
 # Plot with all trips in grey in the background and today's trip in colour
 focus_today <- ggplot(biketrips, aes(Longitude, Latitude)) + geom_point() + 
   geom_point(data = today_trip, aes(Longitude, Latitude, colour = Date)) + 
-  theme(legend.position="none") + coord_map()
+  theme(legend.position="none") + coord_map() + 
+  scale_colour_viridis(direction = -1, discrete = TRUE, alpha = 0.5)
 focus_today
 ggsave("today_overlay.png", dpi = 300, width = 8, height  = 6)
 
