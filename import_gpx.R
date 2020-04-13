3ad0de2fbf27d55665d92472907976ff31ad6427
Daley Mikalson <daley.mikalson@gmail.com>
2020-04-13 16:33:57 -0400

    Clean up code

--- import_gpx.R
+++ import_gpx.R
@@ -22,15 +22,19 @@ parse_GPX <- function(filename) {
 gpx <- map_df(paste0(folder, file_list), parse_GPX)
 
 names(gpx) <- c("X", "date_time", "latitude", "longitude")
-gpx$date_time <- format(ymd_hms(gpx$date_time), tz = "America/Montreal", usetz = TRUE)
+gpx$date_time <- format(ymd_hms(gpx$date_time), 
+                        tz = "America/Montreal", usetz = TRUE)
 gpx$time <- format(as.POSIXct(strptime(gpx$date_time,
                                        "%Y-%m-%d %H:%M",
                                        tz="America/Montreal")),
                    format = "%H:%M:%S")
-gpx$date <- format(as.POSIXct(strptime(gpx$date_time,"%Y-%m-%d %H:%M",tz="America/Montreal")) ,format = "%Y-%m-%d")
+gpx$date <- format(as.POSIXct(strptime(gpx$date_time,"%Y-%m-%d %H:%M",
+                                       tz="America/Montreal")),
+                   format = "%Y-%m-%d")
 
 gpx$tz <- "America/Montreal"
-gpx <- gpx %>% select(date, time, latitude, longitude, X, date_time, tz) %>%
+gpx <- gpx %>% 
+  select(date, time, latitude, longitude, X, date_time, tz) %>%
   mutate(latitude = as.numeric(latitude),
          longitude = as.numeric(longitude),
          date_time = as.POSIXct(date_time))
@@ -52,14 +56,18 @@ todays_ride
 
 # Plot with all trips in grey in the background and today's trip in colour
 # limit to Montreal
-focus_today <- biketrips %>% filter(tz == "America/Montreal") %>%
+focus_today <- biketrips %>% 
+  filter(tz == "America/Montreal") %>%
   ggplot(aes(longitude, latitude)) + 
   geom_point(size = 0.7) + 
-  geom_point(data = today_trip, aes(longitude, latitude, colour = date), size = 0.7) + 
+  geom_point(data = today_trip, 
+             aes(longitude, latitude, colour = date), 
+             size = 0.7) + 
   theme_void() +
   theme(legend.position="none") + 
   coord_map() + 
   scale_colour_viridis_d(direction = -1)
 focus_today
-ggsave(paste0(format(today(), "%Y-%m-%d"),"_overlay.png"), dpi = 300, width = 8, height  = 6)
+ggsave(paste0(format(today(), "%Y-%m-%d"),"_overlay.png"), 
+       dpi = 300, width = 8, height  = 6)
 
