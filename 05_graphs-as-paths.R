8bc6abd1c128095c430fce88d7131b157dbcaa14
Daley Mikalson <daley.mikalson@gmail.com>
2026-08-13 23:10:57 -0400

    Reorganize to plot multiple coloured rides per map.

--- 05_graphs-as-paths.R
+++ 05_graphs-as-paths.R
@@ -35,6 +35,7 @@ gpx <- map_df(paste0(folder, new_list), parse_GPX) |>
   mutate(file = str_remove(file, folder)) |> 
   select(1:2, 4:6, 3)
 
+last_date = max(biketrips$date_time)
 names(gpx) <- c("X", "date_time", "latitude", "longitude", "file", "heart_rate")
 #tz_use = "America/Montreal"
 
@@ -71,18 +72,19 @@ biketrips2 <- biketrips |>
 # Get subset of today's trips only
 biketrips2 <- biketrips2 |>
   #mutate(today = as.factor(if_else(date == Sys.Date(), 1, 0)))
-  mutate(today = as.factor(if_else(file %in% new_list, 1, 0)))
+  #mutate(today = as.factor(if_else(file %in% new_list, 1, 0)))
+  mutate(today = as.factor(if_else(file %in% new_list, date, "0")))
 
 # Plot just today's trip
 todays_ride_path <- biketrips2 |>
-  filter(today == 1) |>
+  filter(today != "0") |>
   ggplot(aes(longitude, latitude, group=time)) +
   geom_path() +
   coord_map()
 todays_ride_path
 
 limits <- biketrips2 |>
-  filter(today == 1) |>
+  filter(today != "0") |>
   summarise(
     min_lon = min(longitude),
     max_lon = max(longitude),
@@ -94,42 +96,45 @@ limits <- biketrips2 |>
 # Plot with all trips in grey in the background and today's trip in colour
 # limit to Montreal
 focus_today_path <- biketrips2 |>
-  filter(tz == "America/Montreal", today == 0) |>
+  filter(tz == "America/Montreal", today == "0") |>
   ggplot(aes(longitude, latitude, colour = today, group = file)) +
   geom_path() +
-  geom_path(data=filter(biketrips2, today ==1), aes(longitude, latitude, colour = today, group = time)) +
+  geom_path(data=filter(biketrips2, today != "0"), aes(longitude, latitude, colour = today, group = file)) +
   theme_void() +
-  theme(legend.position = "none") +
+  #theme(legend.position = "none") +
   coord_sf(crs = 4269) +
-  ggthemes::scale_color_colorblind() +
+  ggthemes::scale_color_colorblind(name = "Date") +
   annotation_north_arrow(location = "tl", which_north = "true",
                          height = unit(0.7, "cm"),
                          width = unit(0.7, "cm"),
                          pad_x = unit(0.4, "in"), pad_y = unit(1, "in"),
-                         style = north_arrow_orienteering) + 
+                         style = north_arrow_orienteering) +
   NULL
 
 focus_today_path
 ggsave(paste0(format(today(), "%Y-%m-%d"), "_overlay.png"),
-  dpi = 300, width = 8, height = 6
+       dpi = 300, width = 8, height = 6
 )
 
 biketrips2 |>
   filter(tz == "America/Montreal", today == 0) |>
   ggplot(aes(longitude, latitude, colour = today, group = file)) +
   geom_path() +
-  geom_path(data=filter(biketrips2, today ==1), aes(longitude, latitude, colour = today, group = time)) +
+  geom_path(data=filter(biketrips2, today != "0"), aes(longitude, latitude, colour = today, group = file)) +
   theme_void() +
-  theme(legend.position = "none") +
+  theme(legend.position = "bottom") +
   coord_sf(crs = 4269) +
-  ggthemes::scale_color_colorblind() + 
-  xlim(limits$min_lon[1] - 0.02, limits$max_lon[1] + 0.02) +
+  ggthemes::scale_color_colorblind(name = "Date") +
+  scale_linetype_discrete(name = "Date") +
+  xlim(limits$min_lon[1] - 0.0075, limits$max_lon[1] + 0.0075) +
   ylim(limits$min_lat[1] - 0.005, limits$max_lat[1] + 0.005) +
   annotation_north_arrow(location = "bl", which_north = "true",
                          height = unit(0.7, "cm"),
                          width = unit(0.7, "cm"),
-                         pad_x = unit(0.4, "in"), pad_y = unit(.5, "in"),
-                         style = north_arrow_orienteering)
+                         pad_x = unit(0.75, "in"), pad_y = unit(.35, "in"),
+                         style = north_arrow_orienteering) +
+  guides(color=guide_legend(nrow=2,byrow=TRUE)) +
+  NULL
 
 ggsave(paste0(format(today(), "%Y-%m-%d"), "_zoom.png"),
   dpi = 300, width = 8, height = 6
